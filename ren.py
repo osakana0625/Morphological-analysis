cf7e2120bd2d8f010e19d64d8486ae49a7f58794
osakana0625 <121215037+osakana0625@users.noreply.github.com>
2023-02-24 01:42:33 +0900

    Morphological analysis/形態素解析
    入力した文字を形態素解析するもの

---
+++ ren.py
@@ -0,0 +1,16 @@
+from janome.tokenizer import Tokenizer
+#形態素解析に必要なライブラリ
+ 
+
+print('形態素解析を始めます')    
+
+string = input("文字を入力してください")
+#入力した文字を「string」に格納
+
+t = Tokenizer()
+#Tokenzierのインスタンスを変数「t」とする
+tokens = t.tokenize(string)
+#形態素形跡実行・メソッドに対象の文字列を渡す？
+ 
+for token in tokens:
+    print(token)
